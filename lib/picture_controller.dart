1a94db3b866b7db3889bc5ec15e02c9737f796b0
Arthur <arturocervantesgalvan@gmail.com>
2021-01-13 11:30:21 -0600

    add controller method

--- lib/picture_controller.dart
+++ lib/picture_controller.dart
@@ -1,12 +1,17 @@
 import 'camerawesome_plugin.dart';
+import './models/sensors.dart';
 
 class PictureController {
   Future<void> takePicture(String filePath) async {
     await CamerawesomePlugin.takePhoto(filePath);
   }
 
+  Future<bool> init() async{
+    return await CamerawesomePlugin.init(Sensors.BACK, false);
+  }
+
   Future<bool> checkPermissions() async{
-    await CamerawesomePlugin.checkPermissions();
+    return await CamerawesomePlugin.checkPermissions();
   }
 
   Future<void> refresh() async{
