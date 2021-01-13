a59c4a538c55bfd68d04209388b84cf5c9489564
Arthur <arturocervantesgalvan@gmail.com>
2021-01-13 09:16:08 -0600

    not close session

--- android/src/main/java/com/apparence/camerawesome/CameraPicture.java
+++ android/src/main/java/com/apparence/camerawesome/CameraPicture.java
@@ -105,7 +105,7 @@ public class CameraPicture implements CameraSession.OnCaptureSession, CameraSett
         }
         if(pictureImageReader == null){
             pictureImageReader = ImageReader.newInstance(size.getWidth(), size.getHeight(), ImageFormat.JPEG, 2);
-            mCameraSession.addPictureSurface(pictureImageReader.getSurface());
+            //mCameraSession.addPictureSurface(pictureImageReader.getSurface());
             Log.e(TAG,"Niless test");
         }
         pictureImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
